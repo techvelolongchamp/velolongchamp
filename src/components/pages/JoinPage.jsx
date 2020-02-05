daa2f5b4d87d5f71a4f948e6c6baca92dabe9b8a
Benoit Goupilleau <benoit.goupilleau@gmail.com>
2020-02-05 16:23:16 +0100

    Added loader

--- src/components/pages/JoinPage.jsx
+++ src/components/pages/JoinPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link, graphql, useStaticQuery } from 'gatsby'
 import styled from 'styled-components'
 
 import Layout from '../Layout'
@@ -7,77 +6,25 @@ import Header from '../Header'
 import Head from '../Head'
 import ThirdarySection from '../sections/ThirdarySection'
 
-const SubTitle = styled.p`
-  font-size: ${({ theme }) => theme.fonts.large};
-  margin: ${({ theme }) => theme.spacing.l} 0;
-  color: ${({ theme }) => theme.colors.brandTertiary};
-  font-weight: 600;
+const FrameContainer = styled.div`
+  background: url(..//loader.gif) center center no-repeat;
 `
 
-const Posts = styled.ol`
-  list-style-type: none;
-  margin: 0;
-`
-
-const Post = styled.li`
-  margin: 20px 0;
-
-  a {
-    background: #f4f4f4;
-    color: #000000;
-    display: block;
-    padding: 20px;
-    text-decoration: none;
-  }
-
-  a:hover {
-    background: #e4e4e4;
-  }
-
-  h2 {
-    margin-bottom: 0;
-  }
-
-  p {
-    color: #777777;
-    font-size: 12px;
-    font-style: italic;
-  }
-`
-
-const BlogPage = ({ lng }) => {
-  const data = useStaticQuery(graphql`
-    query {
-      allMarkdownRemark(
-        sort: { fields: frontmatter___date, order: DESC }
-        limit: 10
-      ) {
-        edges {
-          node {
-            frontmatter {
-              title
-              slug
-              date(locale: "fr", formatString: "Do MMMM YYYY")
-            }
-            excerpt
-          }
-        }
-      }
-    }
-  `)
-
+const JoinFrame = ({ lng }) => {
   return (
     <Layout lng={lng}>
       <Header noScroll lng={lng} />
       <Head title="Blog" url="/blog" lng={lng} />
       <ThirdarySection title="Blog" useH1>
-        <iframe
-          id="haWidget"
-          allowtransparency="true"
-          scrolling="auto"
-          src="https://www.helloasso.com/associations/velo-longchamp/adhesions/adherer-gratuitement-a-velo-longchamp-pour-la-renovation-de-l-anneau-cycliste/widget"
-          style={{ width: '100%', height: '750px', border: 'none' }}
-        ></iframe>
+        <FrameContainer>
+          <iframe
+            id="haWidget"
+            allowtransparency="true"
+            scrolling="auto"
+            src="https://www.helloasso.com/associations/velo-longchamp/adhesions/adherer-gratuitement-a-velo-longchamp-pour-la-renovation-de-l-anneau-cycliste/widget"
+            style={{ width: '100%', height: '750px', border: 'none' }}
+          ></iframe>
+        </FrameContainer>
         <div style={{ width: '100%', textAlign: 'center' }}>
           Propulsé par{' '}
           <a href="https://www.helloasso.com" rel="nofollow">
@@ -89,4 +36,4 @@ const BlogPage = ({ lng }) => {
   )
 }
 
-export default BlogPage
+export default JoinFrame
