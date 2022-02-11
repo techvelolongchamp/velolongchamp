d04d54cce4c4a88db30a193208dd1671add2bd54
Benoit Goupilleau <benoit.goupilleau@gmail.com>
2022-02-11 22:49:14 +0100

    feat: added events

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -2,11 +2,9 @@ import React, { useMemo } from 'react'
 import { useIntl } from 'react-intl'
 import { useRouter } from 'next/router'
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { GetStaticProps } from 'next'
 
-import format from 'date-fns/format'
-import parse from 'date-fns/parse'
-import startOfWeek from 'date-fns/startOfWeek'
-import getDay from 'date-fns/getDay'
+import { format, parse, startOfWeek, getDay } from 'date-fns'
 import enGB from 'date-fns/locale/en-GB'
 import frFR from 'date-fns/locale/fr'
 
@@ -15,8 +13,18 @@ import Layout from '../components/ui/Layout'
 import Header from '../components/ui/Header'
 
 import ThirdarySection from '../components/sections/ThirdarySection'
+import { getAllEvents, ForestryEvent, CalendarEvents } from '../utils/calendar'
 
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+export const formatEvents = (forestryEvent: ForestryEvent): CalendarEvents => {
+  return {
+    ...forestryEvent,
+    start: new Date(forestryEvent.startDate),
+    end: new Date(forestryEvent.endDate),
+    allDay: !!forestryEvent.all_day,
+  }
+}
 
 const locales = {
   'en-GB': enGB,
@@ -31,9 +39,12 @@ const localizer = dateFnsLocalizer({
   locales,
 })
 
-const styles = { height: 'calc(100vh - 230px);' }
+const styles = { height: 'calc(100vh - 230px)' }
 
-const CalendarPage = () => {
+const CalendarPage: React.FC<{ rawEvents: ForestryEvent[] }> = ({
+  rawEvents,
+}) => {
+  const events = rawEvents.map((e) => formatEvents(e))
   const { locale } = useRouter()
   const { formatMessage } = useIntl()
 
@@ -75,9 +86,10 @@ const CalendarPage = () => {
           startAccessor="start"
           endAccessor="end"
           culture={culture}
-          events={[]}
+          events={events}
           style={styles}
           messages={messages}
+          popup
         />
       </ThirdarySection>
     </Layout>
@@ -85,3 +97,11 @@ const CalendarPage = () => {
 }
 
 export default CalendarPage
+
+export const getStaticProps: GetStaticProps = async () => {
+  const rawEvents = getAllEvents()
+
+  return {
+    props: { rawEvents },
+  }
+}
---
+++ utils/calendar.ts
@@ -0,0 +1,92 @@
+import fs from 'fs'
+import path from 'path'
+import { sub, add, startOfDay, endOfDay, nextDay } from 'date-fns'
+
+const calendarDirectory = path.join(process.cwd(), 'calendar')
+
+const now = new Date()
+const minDate = startOfDay(sub(now, { months: 2 }))
+const maxDate = endOfDay(add(now, { years: 1 }))
+
+export function getEventsFiles() {
+  return fs.readdirSync(calendarDirectory).filter((el) => {
+    const extName = path.extname(el)
+    return extName === '.json'
+  })
+}
+
+export type ForestryEvent = {
+  organizer: string
+  title: string
+  description: string
+  startDate: string
+  endDate: string
+  recurrent: boolean
+  repeated_day?: (keyof typeof Days)[]
+  all_day?: boolean
+  end_date_repeat?: string
+}
+
+export type CalendarEvents = {
+  title: string
+  start: Date
+  end: Date
+  allDay?: boolean
+  resource?: string
+  description: string
+}
+
+enum Days {
+  Lundi = 1,
+  Mardi = 2,
+  Mercredi = 3,
+  Jeudi = 4,
+  Vendredi = 5,
+  Samedi = 6,
+  Dimanche = 0,
+}
+
+const repeatEvents = (rawEvents: ForestryEvent[]) => {
+  const events = [...rawEvents]
+  rawEvents.forEach((e) => {
+    if (e.recurrent && e.repeated_day && e.repeated_day.length > 0) {
+      const maxEventDate = e.end_date_repeat
+        ? endOfDay(new Date(e.end_date_repeat))
+        : maxDate
+      e.repeated_day.forEach((day) => {
+        const dayNumber = Days[day]
+        let nextStartDate = nextDay(new Date(e.startDate), dayNumber)
+        let nextEndDate = nextDay(new Date(e.endDate), dayNumber)
+        while (nextStartDate < maxEventDate) {
+          const nextEvent = {
+            ...e,
+            startDate: nextStartDate.toISOString(),
+            endDate: nextEndDate.toISOString(),
+          }
+          events.push(nextEvent)
+          nextStartDate = nextDay(nextStartDate, dayNumber)
+          nextEndDate = nextDay(nextEndDate, dayNumber)
+        }
+      })
+    }
+  })
+
+  return events
+}
+
+export function getAllEvents() {
+  const files = getEventsFiles()
+
+  const rawEvents = files.map((f) => {
+    const fullPath = path.join(calendarDirectory, f)
+    const rawData = fs.readFileSync(fullPath, 'utf8')
+
+    return JSON.parse(rawData)
+  })
+
+  const events = repeatEvents(rawEvents)
+
+  return events.filter(
+    (e) => new Date(e.endDate) > minDate || new Date(e.startDate) < maxDate
+  )
+}
